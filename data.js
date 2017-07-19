e820fe709b058b7424fa0deb02c372f5c0e377eb
Hardik <missileh@gmail.com>
2017-07-19 17:58:22 +0530

    Hardik - First Working version. Click NEXT to go to next link. Each link shows facebook group with search-text.

---
+++ data.js
@@ -0,0 +1,10 @@
+var linksData = JSON.parse('{ 	\
+  "facebook_groups": [         \
+    "TheCrazyProgrammers",        \
+    "1286952051356674"     \
+  ],						   \
+  "search_queries": [          \
+    "C++",                \
+    "new at programming",             \
+  ]                           \
+}');
